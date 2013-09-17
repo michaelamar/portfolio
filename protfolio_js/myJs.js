a4bb7e0056d7285443e406799713231e55a5f1b5
Michael Amar <Mordachiamar@gmail.com>
2013-09-17 13:59:07 -0400

    portfolio basic layout

---
+++ protfolio_js/myJs.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$('.home_text').waypoint(function() {
+		$('#home').css("background-color", "red");
+	});
+
+
+	$('.resume').waypoint(function() {
+		$('#resume').css("background-color", "red");
+	});
+
+	$('.work').waypoint(function() {
+		$('#work').css("background-color", "red");
+	});
+
+	$('.contact').waypoint(function() {
+		$('#contact').css("background-color", "red");
+	});
+
+	$('.reading').waypoint(function() {
+		$('#reading').css("background-color", "red");
+	});
+
+});
