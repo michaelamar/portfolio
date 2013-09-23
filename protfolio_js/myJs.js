273c882aa03e19c5d132c73acf3520f50c0051ab
Michael Amar <Mordachiamar@gmail.com>
2013-09-22 21:09:42 -0400

    design touched up

--- protfolio_js/myJs.js
+++ protfolio_js/myJs.js
@@ -1,23 +1,34 @@
 $(document).ready(function(){
 	$('.home_text').waypoint(function() {
+		clearAllBack();
 		$('#home').css("background-color", "red");
 	});
 
 
 	$('.resume').waypoint(function() {
+		clearAllBack();
 		$('#resume').css("background-color", "red");
 	});
 
 	$('.work').waypoint(function() {
+		clearAllBack();
 		$('#work').css("background-color", "red");
 	});
 
 	$('.contact').waypoint(function() {
+		clearAllBack();
 		$('#contact').css("background-color", "red");
+
 	});
 
 	$('.reading').waypoint(function() {
+		clearAllBack();
 		$('#reading').css("background-color", "red");
+		$('.reading').css("background-color", "green");
 	});
 
+	function clearAllBack(){
+		$('#resume, #home, #work, #reading, #contact').css("background-color", "white");
+		
+	}
 });
