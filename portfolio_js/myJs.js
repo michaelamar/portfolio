15a008d3184fb3c3053e5c7f93d236fa7cb55953
Michael Amar <Mordachiamar@gmail.com>
2013-09-30 22:43:41 -0400

    finally a decent working version

--- portfolio_js/myJs.js
+++ portfolio_js/myJs.js
@@ -1,50 +1,53 @@
 $(document).ready(function(){
 	$('.home_text').waypoint(function() {
-		clearAll();
-		$('#home').css({"transition": "ease color 1s", "color": "#ff1166"});
-		$(this).css({"background-color": "#0FFFD7"});
-	}, { offset: '15%' });
+		//clearAll();
+		$('#home').toggleClass('clear');
+		$('html').css({"transition": "ease background-color 1s", "background-color": "#22B3C9"});
+	}, { offset: '7%' });
 
 	$('.reading').waypoint(function() {
-		clearAll();
-		$('#reading').css({"color": "#ff1166"});
-		$(this).css({"background-color": "#0033FF"});
+		//clearAll();
+		$('#home').toggleClass('clear');
+		$('#reading').toggleClass('clear');
+		$('html').css({"transition": "ease background-color 1s", "background-color": "#FF8651"});
 		//$('.reading').css("background-color", "green");
-	}, { offset: '15%' });
+	}, { offset: '20%' });
 
 
 	$('.resume').waypoint(function() {
-		clearAll();
-		
-		$('#resume').css({"color": "#ff1166"});
-		$('.work').css({"background-color": "transparent"});
-		$('body').css({"background-color": "#999"});
+		//clearAll();
+		$('#reading').toggleClass('clear');
+		$('#resume').toggleClass('clear');
+		//$('.work').css({"background-color": "transparent"});
+		$('html').css({"transition": "ease background-color 1s", "background-color": "#22B3C9"}); //CC3D22"
 
 		 
-	}, { offset: '15%' });
+	}, { offset: '20%' });
 
 
 	$('.work').waypoint(function() {
-		clearAll();
-		$('#work').css({"color": "#ff1166"});
-		$('.contact').css({"background-color": "transparent"});
-		$('body').css({"background-color": "#FF510F"});
+		//clearAll();
+		$('#resume').toggleClass('clear');
+		$('#work').toggleClass('clear');
+		//$('.contact').css({"background-color": "transparent"});
+		$('html').css({"transition": "ease background-color 1s", "background-color": "#FF510F"});
 
-	}, { offset: '15%' });
+	}, { offset: '20%' });
 
 	$('.contact').waypoint(function() {
-		clearAll();
-		$('#contact').css({"color": "#ff1166"});
-		$('body').css({"background-color": "#FFCC00"});
+		//clearAll();
+		$('#work').toggleClass('clear');
+		$('#contact').toggleClass('clear');
+		$('html').css({"transition": "ease background-color 1s", "background-color": "#FFCC00"});
 
 
-	}, { offset: '15%' });
+	}, { offset: '20%' });
 
 //problem is it gets in the way of my hover
-	function clearAll(){
-		$('#resume, #home, #work, #reading, #contact').css({"color":"#11ddff"});
-		//$('.block').css({"background-color":"white"});
-	}
+	// function clearAll(){
+	// 	$('#resume, #home, #work, #reading, #contact').css({"color":"#11ddff"});
+	// 	//$('.block').css({"background-color":"white"});
+	// }
 
 	// function createWaypoint(clas, id){
 	// $(clas).waypoint(function(){
